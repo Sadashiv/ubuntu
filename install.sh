0a9b22c6e359d135c77cffde589904fbeb719a72
Sadashiv H <sadamsrit@gmail.com>
2021-06-26 16:32:29 +0530

    Added ansible install steps

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #sudo visudoers
 sudo apt-get update -y
+#update-manager -p #Update the latest version
+#sudo do-release-upgrade #Upgrade the 1.0LTS to 2.0LTS version
+
 google_chrome=`dpkg -l google-chrome-stable`
 if [[ $google_chrome =~ "google-chrome-stable" ]];
 then
@@ -9,9 +12,12 @@ else
     sudo dpkg -i google-chrome-stable_current_amd64.deb
 fi
 
-sudo sudo apt install -y vlc git vim  wget gcc libncursesw5-dev libgdbm-dev libc6-dev libsqlite3-dev tk-dev \
-libssl-dev openssl zlib1g-dev libffi-dev libmysqlclient-dev libsasl2-dev python-dev libldap2-dev libbz2-dev \
-default-jdk net-tools nginx
+for installtype in install upgrade
+do
+    sudo sudo apt $installtype -y vlc git vim  wget gcc libncursesw5-dev libgdbm-dev libc6-dev libsqlite3-dev tk-dev \
+    libssl-dev openssl zlib1g-dev libffi-dev libmysqlclient-dev libsasl2-dev python-dev libldap2-dev libbz2-dev \
+    default-jdk net-tools nginx google-chrome-stable
+done
 
 #wget -c https://github.com/Sadashiv/interview_questions/blob/master/docker/get-docker-ce.sh
 #chmod +x get-docker-ce.sh
@@ -19,8 +25,26 @@ default-jdk net-tools nginx
 docker_install=`dpkg -l docker`
 if [[ $docker_install =~ "docker" ]];
 then
-    echo "Docker already installed"
+    echo "Docker already installed `docker --version`"
 else
     ./get-docker-ce.sh
     sudo usermod -aG docker sada
+fi
+
+ansible_install=`dpkg -l ansible`
+if [[ $ansible_install =~ "ansible" ]];
+then
+    echo "Ansible already installed `ansible --version`"
+else
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt install ansible -y 
+fi
+
+minikube_install=`dpkg -l minikube`
+if [[ $minikube_instal =~ "minikube" ]];
+then
+    echo "Minikube alread installed `minikube version`"
+else
+    wget -c https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+    sudo dpkg -i minikube_latest_amd64.deb
 fi
