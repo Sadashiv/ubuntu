5b6c8ed613667972e23c6ed277f32a6b243e0b0a
Sadashiv H <sadamsrit@gmail.com>
2021-06-27 11:56:53 +0530

    install docker-compose

--- install.sh
+++ install.sh
@@ -41,10 +41,20 @@ else
 fi
 
 minikube_install=`dpkg -l minikube`
-if [[ $minikube_instal =~ "minikube" ]];
+if [[ $minikube_install =~ "minikube" ]];
 then
     echo "Minikube alread installed `minikube version`"
 else
     wget -c https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
     sudo dpkg -i minikube_latest_amd64.deb
+fi
+
+docker_compose_install=`docker-compose`
+if [[ $? -eq 0 ]];
+then
+    echo "docker-compose alread installed `docker-compose version`"
+else
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
